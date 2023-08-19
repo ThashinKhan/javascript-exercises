ccb9166cf6f2da03d509147b5986f7b5fa249e00
Thashin Khan <123204598+ThashinKhan@users.noreply.github.com>
2023-08-19 01:25:08 -0400

    Finished Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(rev) {
+    let revTemp;
+    if(rev == ''){
+        return '';
+    }
+    else{
+        for(let i = rev.length; i > 0; i--){
+            if(i == rev.length) revTemp = rev[i - 1];
+            else revTemp += rev[i - 1];
+        }
+        return revTemp;
+    }
 };
 
 // Do not edit below this line

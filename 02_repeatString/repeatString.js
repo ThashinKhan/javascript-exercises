27e5a6d3e2ae8509ffc85f382ecfa113292fba61
Thashin Khan <123204598+ThashinKhan@users.noreply.github.com>
2023-08-19 01:16:08 -0400

    Finished Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(rep, num) {
+    let originalString = rep;
 
+    if(num <= 0){
+        if(num == 0) return '';
+        else return 'ERROR';
+    }
+    else{
+        for(let i = 0; i < num - 1; i++){
+            rep += originalString;
+        }
+        return rep;
+    }
 };
 
 // Do not edit below this line

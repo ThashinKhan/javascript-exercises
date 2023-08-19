ad7ee576887c68774125779e732ddfb36c754822
Thashin Khan <123204598+ThashinKhan@users.noreply.github.com>
2023-08-19 02:19:28 -0400

    Finished Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    //ensures correct result when end is larger than start.
+    if(start > end){
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    
+    //check negative or non-number inputs.
+    if(start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number'){
+        return "ERROR";
+    }
 
+
+    for(let i = start + 1; i <= end; i++){
+        start += i;
+    }
+    return start;
 };
 
 // Do not edit below this line

6ad22d06b52cd441002ff19b53cfd9ec395d04b3
Thashin Khan <123204598+ThashinKhan@users.noreply.github.com>
2023-08-19 02:29:16 -0400

    Finished Exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //obvious case is if its divisible by 4 but not 100.
+    if(year % 4 == 0 && year % 100 !== 0){
+        return true;
+    }
 
+    else if(year % 400 == 0){
+        return true 
+    }
+
+    //if all cases fail, not a leap year.
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
